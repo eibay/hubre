1686134235ddcec1ec76ce9388929edb9d315581
Elmer Ibayan <eibayan@gmail.com>
2015-08-26 15:00:42 +1000

    started on saving new property portion

--- app_propertypro/public/js/app.js
+++ app_propertypro/public/js/app.js
@@ -87,6 +87,7 @@ $(document).ready(function(){
         content : "<p><a href='#'>Edit Details</a></p>"
       }
     });
+    showNewPropertyForm();
   });
 });
 
@@ -194,3 +195,31 @@ function initAutocomplete() {
   // });
 // }
 
+//Adding New Property
+var newProperty = function(){
+  var label = $('#property-label').val();
+  var address = $('#property-address').val();
+
+  var data = {
+    property: {
+      label: label,
+      address: address
+    }
+  }
+
+  $.ajax({
+    url: 'http://localhost:3000/properties',
+    data: data,
+    type: 'post'
+
+  }).done(function(){
+    alert('success saving');
+  });
+}
+
+$('#new-property-btn').on('click', newProperty);
+
+//SHOW NEW PROPERTY FORM
+var showNewPropertyForm = function(){
+  $('#new-property-form').css('display', 'inline');
+}
--- propertypro/app/controllers/properties_controller.rb
+++ propertypro/app/controllers/properties_controller.rb
@@ -5,10 +5,17 @@ class PropertiesController < ApplicationController
 		render json: properties
 	end
 
-	def create
-	end
+  def create
+    propertie = Propertie.new(propertie_params)
+    if propertie.save
+      render json: propertie.to_json, status: 201
+    else
+      render json: { message: 'not ok'}.to_json, status: 422
+    end
+  end
 
-	def propertie_params
-	end
+  def propertie_params
+    params.require(:propertie).permit(:label, :address)
+  end
 
 end
